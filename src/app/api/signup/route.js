18b873c7f3d3d5780de2306d70433a440c4cffc1
MRA-goosebumps <hazedivision127@gmail.com>
2025-06-25 08:34:06 +0700

    updateSecure

--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -17,11 +17,11 @@ export async function POST(request) {
 
     // Koneksi ke database
     const connection = await mysql.createConnection({
-      host: "sql12.freesqldatabase.com",
-      user: "sql12785645",
-      password: "nFMfH2FRqz",
-      database: "sql12785645",
-      port: 3306,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
     });
 
     // Simpan data ke database
